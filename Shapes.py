9de6e4dd539857cf079af98faed9dba8b45f0118
hduffek <hduffek2@yahoo.com>
2022-04-29 16:52:57 -0500

    Overhauled Shapes.py Classes and wrote test_Shapes.py to full functionality

--- Shapes.py
+++ Shapes.py
@@ -1,91 +1,99 @@
 import math
 
 
-class Shapes:
+class Circle:
     """
-    Class for calculating the area of Circles, Squares, Triangle, and rectangles
+    Class to calculate the area of a circle, given the radius
+    :return: Area of circle
     """
+    def __init__(self, radius):
+        self.radius = radius
 
-    def __init__(self, radius, side, length, width, height, base):
-        """
-        Initialization of different variables within functions of Shapes
-        :param radius: Radius of circle
-        :param side: Side of square
-        :param length: Length of side for rectangle
-        :param width: Width of side for rectangle
-        :param height: Height of triangle
-        :param base: Base length of triangle
-        """
-        self.__radius = radius
-        self.__side = side
-        self.__length = length
-        self.__width = width
-        self.__height = height
-        self.__base = base
-
-    def circle(self) -> int or float:
-        """
-        Function to calculate the area of a circle, given the radius
-        :return: Area of circle
-        """
-        if type(self.__radius) != int and type(
-                self.__radius) != float:  # Checks for non-integer and non-float type inputs
+    def circ_area(self):
+        if type(self.radius) != int and type(
+                self.radius) != float:  # Checks for non-integer and non-float type inputs
             raise TypeError('Not numeric input')
 
-        if self.__radius <= 0:  # Checks if radius is less than or equal to zero
-            if self.__radius == 0:  # Raises ValueError when radius is zero
+        if self.radius <= 0:  # Checks if radius is less than or equal to zero
+            if self.radius == 0:  # Raises ValueError when radius is zero
                 raise ValueError('zero input')
             else:  # Raises Value error when radius is negative
                 raise ValueError('negative input')
 
-        return math.pi * (self.__radius * self.__radius)
+        return math.pi * (self.radius * self.radius)
 
-    def square(self) -> int or float:
-        """
-        Function to calculate the area of a square, given the side length
-        :return: Area of square
-        """
-        if type(self.__side) != int and type(self.__side) != float:   # Checks for non-integer and non-float type inputs
+
+class Square:
+    """
+    Class to calculate the area of a square, given the side length
+    :return: Area of square
+    """
+    def __init__(self, side):
+        self.side = side
+
+    def sq_area(self):
+        if type(self.side) != int and type(self.side) != float:   # Checks for non-integer and non-float type inputs
             raise TypeError('Not numeric input')
 
-        if self.__side <= 0:   # Checks if side length is less than or equal to zero
-            if self.__side == 0:   # Raises ValueError when side is zero
+        if self.side <= 0:   # Checks if side length is less than or equal to zero
+            if self.side == 0:   # Raises ValueError when side is zero
                 raise ValueError('zero input')
             else:   # Raises ValueError when side is negative
                 raise ValueError('negative input')
 
-        return pow(self.__side, 2)
+        return pow(self.side, 2)
 
-    def rectangle(self) -> int or float:
-        """
-        Function to calculate the area of a rectangle, given the length and width
-        :return: Area of rectangle
-        """
-        if (type(self.__length) != int and type(self.__length) != float) or (
-                type(self.__width) != int and type(self.__width) != float):   # Checks for incorrect input types
+
+class Rectangle:
+    """
+    Class to calculate the area of a rectangle, given the length and width
+    :return: Area of rectangle
+    """
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def rect_area(self):
+        if (type(self.length) != int and type(self.length) != float) or (
+                type(self.width) != int and type(self.width) != float):   # Checks for incorrect input types
             raise TypeError('Not numeric input')
 
-        if (self.__length <= 0) or (self.__width <= 0):   # Checks for negative values
-            if (self.__length == 0) or (self.__width == 0):   # Raises ValueError for inputs equal to zero
+        if (self.length <= 0) or (self.width <= 0):   # Checks for negative values
+            if (self.length == 0) or (self.width == 0):   # Raises ValueError for inputs equal to zero
                 raise ValueError('zero input')
             else:   # Raises ValueError for negative input values
                 raise ValueError('negative input')
 
-        return self.__length * self.__width
+        return self.length * self.width
 
-    def triangle(self) -> int or float:
-        """
-        Function to calculate the area of a triangle, given the base and height
-        :return: Area of triangle
-        """
-        if (type(self.__base) != int and type(self.__base) != float) or (
-                type(self.__height) != int and type(self.__height) != float):   # Checks for incorrect variable types
+
+class Triangle:
+    """
+    Class to calculate the area of a triangle, given the base and height
+    :return: Area of triangle
+    """
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def tri_area(self):
+        if (type(self.base) != int and type(self.base) != float) or (
+                type(self.height) != int and type(self.height) != float):   # Checks for incorrect variable types
             raise TypeError('Not numeric input')
 
-        if (self.__base <= 0) or (self.__height <= 0):   # Checks for negative values
-            if (self.__base == 0) or (self.__height == 0):   # Raises ValueError for inputs equal to zero
+        if (self.base <= 0) or (self.height <= 0):   # Checks for negative values
+            if (self.base == 0) or (self.height == 0):   # Raises ValueError for inputs equal to zero
                 raise ValueError('zero input')
             else:   # Raises ValueError for negative inputs
                 raise ValueError('negative input')
 
-        return (1 / 2) * self.__base * self.__height
+        return (1 / 2) * self.base * self.height
+
+
+def main():
+    c = Circle(5)
+    print(c.circ_area())
+
+
+if __name__ == '__main__':
+    main()
--- test_Shapes.py
+++ test_Shapes.py
@@ -3,8 +3,102 @@ from Shapes import *
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def test_Circle(self):
+        c = Circle(5)
+        self.assertEqual(c.circ_area(), (math.pi * 25))
+        c = Circle(0.1)
+        self.assertAlmostEqual(c.circ_area(), 0.01, delta=0.1)
+
+        with self.assertRaises(TypeError):
+            c = Circle('0.1')
+            c.circ_area()
+
+        with self.assertRaises(ValueError):
+            c = Circle(-1)
+            c.circ_area()
+            c = Circle(0)
+            c.circ_area()
+
+    def test_square(self):
+        s = Square(10)
+        self.assertEqual(s.sq_area(), 100)
+        s = Square(0.1)
+        self.assertAlmostEqual(s.sq_area(), 0.01, delta=0.1)
+
+        with self.assertRaises(TypeError):
+            s = Square('0.1')
+            s.sq_area()
+
+        with self.assertRaises(ValueError):
+            s = Square(-1)
+            s.sq_area()
+            s = Square(0)
+            s.sq_area()
+
+    def test_rectangle(self):
+        r = Rectangle(5, 2)
+        self.assertEqual(r.rect_area(), 10)
+        r = Rectangle(2.5, 1)
+        self.assertAlmostEqual(r.rect_area(), 2.5, delta=0.1)
+        r = Rectangle(1, 2.5)
+        self.assertAlmostEqual(r.rect_area(), 2.5, delta=0.1)
+        r = Rectangle(2.5, 3.5)
+        self.assertAlmostEqual(r.rect_area(), 8.75, delta=0.1)
+
+        with self.assertRaises(TypeError):
+            r = Rectangle('0.1', 2)
+            r.rect_area()('0.1', 2)
+            r = Rectangle(6, '2')
+            r.rect_area()(6, '2')
+            r = Rectangle('3', '5')
+            r.rect_area()('3', '5')
+
+        with self.assertRaises(ValueError):
+            r = Rectangle(-1, 3)
+            r.rect_area()
+            r = Rectangle(2, -5)
+            r.rect_area()
+            r = Rectangle(-1, -2)
+            r.rect_area()
+            r = Rectangle(0, 1)
+            r.rect_area()
+            r = Rectangle(6, 0)
+            r.rect_area()
+            r = Rectangle(0, 0)
+            r.rect_area()
+
+    def test_triangle(self):
+        t = Triangle(4, 3)
+        self.assertEqual(t.tri_area(), 6)
+        t = Triangle(1.5, 1)
+        self.assertAlmostEqual(t.tri_area(), 0.75, delta=0.1)
+        t = Triangle(1, 1.5)
+        self.assertAlmostEqual(t.tri_area(), 0.75, delta=0.1)
+        t = Triangle(1.5, 3.4)
+        self.assertAlmostEqual(t.tri_area(), 2.55, delta=0.1)
+
+        with self.assertRaises(TypeError):
+            t = Triangle('0.1', 2)
+            t.tri_area()
+            t = Triangle(6, '2')
+            t.tri_area()
+            t = Triangle('3', '5')
+            t.tri_area()
+
+        with self.assertRaises(ValueError):
+            t = Triangle(-1, 3)
+            t.tri_area()
+            t = Triangle(2, -5)
+            t.tri_area()
+            t = Triangle(-1, -2)
+            t.tri_area()
+            t = Triangle(0, 1)
+            t.tri_area()
+            t = Triangle(6, 0)
+            t.tri_area()
+            t = Triangle(0, 0)
+            t.tri_area()
 
 
 if __name__ == '__main__':
